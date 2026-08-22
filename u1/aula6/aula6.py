6ce4d0e6121f658616417ff14b694b449c43467f
CLARA GOMES FIGUEREDO <clara.figueredo@aluno.senac.rio>
2026-08-21 21:16:52 -0300

    analistafiga

---
+++ u1/aula6/aula6.py
@@ -0,0 +1,15 @@
+#for i in range (1,10,2):
+#    print(i)
+
+#while
+
+#somador= int(input("registro:"))
+#controle = 0
+
+#while controle <= 30:   
+#    controle = controle+somador
+
+
+#print ("oficina lotada")
+
+
---
+++ u1/aula6/exemplos.py
@@ -0,0 +1,10 @@
+while acertou <= 5:
+    print (f"numero{acertou +1} de 5:")
+    num= floar (nput("digite um numero:"))
+
+    dobro = num *2
+    triplo= num *3
+    quadruplo - num *4
+
+    print (f"Resultado: Dobro = {dobro}, triplo={triplo}, quadruplo = {quadruplo}")
+    acertou =+1
