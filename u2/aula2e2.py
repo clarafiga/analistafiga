89b8927ca41664c0e4fc17c0c30a9b97555dec7e
CLARA GOMES FIGUEREDO <clara.figueredo@aluno.senac.rio>
2026-09-23 21:25:53 -0300

    atual

--- u2/aula2e2.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-# Carregar a planilha 'Transacoes' para um DataFrame
-df_transacoes = pd.read_excel('base_invest.xlsx', sheet_name='Transacoes')
-# Exibir as primeiras 5 linhas para verificar os dados
-print(df_transacoes.head())
--- u2/aula3.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('vendas_produtos.csv')
-print(df.head())
